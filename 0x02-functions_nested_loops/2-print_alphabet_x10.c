a9ae2e9766b38746ddda616a1ef0c8f2d17a8fe2
Mirabel55 <mirabelchukwuemeka686@gmail.com>
2022-12-14 17:32:09 -0800

    task 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - print 10 times the alphabet, in lower case
+ * print_alphabet_x10 - print 10 times the alphabet, in lowercase
  */
 
 void print_alphabet_x10(void)
